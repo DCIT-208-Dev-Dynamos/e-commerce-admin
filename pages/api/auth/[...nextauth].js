31d794d869278616225424b6016b2386ce83c1a6
cyruskwameblay <cyruskwameblay@hotmail.com>
2023-07-20 11:23:41 +0000

    fixed bugs

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['ephraimmensah99@gmail.com'];
+const adminEmails = ['ephraimmensah99@gmail.com', 'cyruskwameblay@hotmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
